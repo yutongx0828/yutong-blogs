410c38a69fdd127a20de77ad9dfa2386b6f71601
Yutong Xu <yuxu0537@uni.sydney.edu.au>
2025-03-25 01:50:10 +1100

    supabase client made

--- src/database.py
+++ src/database.py
@@ -1,6 +1,14 @@
 import os
 from supabase import create_client, Client
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+class Settings(BaseSettings):
+    supabase_url: str
+    supabase_key: str
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
+url = settings.supabase_url
+key = settings.supabase_key
 supabase: Client = create_client(url, key)
